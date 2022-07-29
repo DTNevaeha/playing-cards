ebbdd295121fa95f8489804e7bef33f95fc5cd56
DTNevaeha <DTNevaeha@gmail.com>
2022-07-29 15:17:49 -0500

    Sort first by rank then by suit

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -2,6 +2,7 @@ package com.tlglearning.cards;
 
 import com.tlglearning.cards.model.Card;
 import com.tlglearning.cards.model.Deck;
+import com.tlglearning.cards.strategy.RankFirstComparator;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -13,9 +14,14 @@ public class TestMain {
     System.out.println(deck);
     deck.shuffle(rng); //shuffle deck randomly
     System.out.println(deck);
-    for (Card card : deck) { //no idea what this does other than print cards in a vertical line
-      System.out.println(card);
-    }
+//    for (Card card : deck) { //no idea what this does other than print cards in a vertical line
+//      System.out.println(card);
+//    }
+    deck.sort();
+    System.out.println(deck); //Sort by rank
+
+    deck.sort(new RankFirstComparator()); //makes it so they are all sorted by suit
+    System.out.println(deck);
   }
 
 }

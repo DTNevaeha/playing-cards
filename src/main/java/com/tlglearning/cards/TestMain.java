0abaf6995f13067b07a16b958701ab9db43d1401
DTNevaeha <DTNevaeha@gmail.com>
2022-08-02 17:08:23 -0500

    Added Spliterator

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -2,28 +2,30 @@ package com.tlglearning.cards;
 
 import com.tlglearning.cards.model.Card;
 import com.tlglearning.cards.model.Deck;
+import com.tlglearning.cards.model.Rank;
 import java.security.SecureRandom;
-import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class TestMain {
 
   public static void main(String[] args) {
-    Deck deck = new Deck();
-    Random rng = new SecureRandom();
-    System.out.println(deck);
-    deck.shuffle(rng); //shuffle deck randomly
-    System.out.println(deck);
-//    for (Card card : deck) { //print cards in a vertical line
-//      System.out.println(card);
-//    }
-
-    deck.sort();
-    System.out.println(deck); //Sort by rank
-
-    deck.sort(Comparator.comparing(Card::getRank).thenComparing(Card::getSuit));
-    System.out.println(deck);
-
+    Deck deck = new Deck(); //creates a new deck
+    Random rng = new SecureRandom();  // creates a random shuffle
+    Set<Rank> exclutedCards = EnumSet.range(Rank.TWO, Rank.EIGHT); //excludes cards 2-8
+    deck.shuffle(rng);  //shuffles the cards randomly
+    Spliterator<Card> splitter = Spliterators.spliterator(deck.iterator(), deck.size(), 0);
+        List<Card> pinochleCards = StreamSupport
+        .stream(splitter, false)
+        .filter((card) -> !exclutedCards.contains(card.getRank())) //allows all cards not excluded.
+        .collect(Collectors.toList());
+    System.out.println(pinochleCards);
   }
 
 }
